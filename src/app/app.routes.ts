46c49fca9c819431d8646147f8097bcac7df37e3
dymafr <erwan1309@hotmail.com>
2020-12-28 18:46:35 +0100

    1 file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { PanierContainerComponent } from "./panier-container/panier-container.component";
 import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
 import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
+import { CocktailFormComponent } from "./cocktail-container/cocktail-form/cocktail-form.component";
 
 export const APP_ROUTES: Routes = [
   { path: "", redirectTo: "cocktails", pathMatch: "full" },
@@ -9,8 +10,8 @@ export const APP_ROUTES: Routes = [
     path: "cocktails",
     component: CocktailContainerComponent,
     children: [
-      { path: "new", component: CocktailEditComponent },
-      { path: ":index/edit", component: CocktailEditComponent },
+      { path: "new", component: CocktailFormComponent },
+      { path: ":index/edit", component: CocktailFormComponent },
 
       { path: ":index", component: CocktailDetailsComponent },
       { path: "", redirectTo: "0", pathMatch: "full" }
