c6b5cc52fdf119e56947d2a62acc3d7f127ffa98
dymafr <erwan1309@hotmail.com>
2021-09-07 15:07:36 +0200

    7 files modified

--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { Cocktail } from "../../shared/interfaces/cocktail.interface";
-import { CocktailService } from "../../shared/services/cocktail.service";
-import { PanierService } from "../../shared/services/panier.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Cocktail } from '../../shared/interfaces/cocktail.interface';
+import { CocktailService } from '../../shared/services/cocktail.service';
+import { PanierService } from '../../shared/services/panier.service';
 
 @Component({
-  selector: "app-cocktail-details",
-  templateUrl: "./cocktail-details.component.html",
-  styleUrls: ["./cocktail-details.component.scss"]
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.scss']
 })
 export class CocktailDetailsComponent implements OnInit {
-  public cocktail: Cocktail;
+  public cocktail?: Cocktail;
 
   constructor(
     private cocktailService: CocktailService,
@@ -20,11 +20,16 @@ export class CocktailDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      this.cocktail = this.cocktailService.getCocktail(+paramMap.get("index"));
+      const cocktailIndex = paramMap.get('index');
+      if (cocktailIndex) {
+        this.cocktail = this.cocktailService.getCocktail(+cocktailIndex);
+      }
     });
   }
 
   public addToPanier() {
-    this.panierService.addPanier(this.cocktail.ingredients);
+    if (this.cocktail) {
+      this.panierService.addPanier(this.cocktail.ingredients);
+    }
   }
 }
